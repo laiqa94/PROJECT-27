77ee7a111614b00105e9c8f1bf61cb640aa02e19
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:31:31 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.ts
@@ -0,0 +1,49 @@
+
+
+let alineColors :string = "green";
+
+//version 1 of the program
+if(alineColors === "green"){
+   console.log("Version 1:player earned 5 point.");
+}
+else if(alineColors === "yellow"){
+    console.log("Player earned 10 point.");
+}
+else if(alineColors === "red"){
+     console.log("Player earned 15 point.");
+}
+else {
+    ("Please select right color")
+}
+
+//version 2 of the program
+alineColors ="yellow";
+
+if(alineColors === "green"){
+    console.log("player earnend 5 point.");
+}
+else if(alineColors === "yellow"){
+    console.log("Version 2:Player earned 10 point.");
+}
+else if(alineColors === "red"){
+    console.log("Player earned 15 point.");
+}
+else {
+    console.log("Please select right color")
+}
+
+//version 3 of the program
+alineColors  ="red";
+
+if(alineColors === "green"){
+    console.log("Player earned 5 point.");
+}
+else if(alineColors === "yellow"){
+    console.log("Player earned 10 point.");
+}
+else if(alineColors === "red"){
+    console.log("Version 3:Player earned 15 point.");
+}
+else{
+    console.log("Please select right color")
+}
